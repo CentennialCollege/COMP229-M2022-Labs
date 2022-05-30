70a94692e00de632c3749d2e63c732e7c3bd8a6d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-05-30 09:33:26 -0400

    added basic routes in Router file

--- Week4/Server/Routes/index.js
+++ Week4/Server/Routes/index.js
@@ -6,7 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About Us', page: 'about' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Our Services', page: 'services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- Week4/Server/Routes/index.ts
+++ Week4/Server/Routes/index.ts
@@ -1,10 +1,40 @@
 import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
+/* Display home page. */
 router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) 
 {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home', page: 'home' });
+});
+
+/* Display home page. */
+router.get('/home', function(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'Home', page: 'home' });
+});
+
+/* Display about page. */
+router.get('/about', function(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'About Us', page: 'about' });
+});
+
+/* Display projects page. */
+router.get('/projects', function(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+
+/* Display services page. */
+router.get('/services', function(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'Our Services', page: 'services' });
+});
+
+/* Display contact page. */
+router.get('/contact', function(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 
 export default router;
