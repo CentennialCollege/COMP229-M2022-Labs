451cfb8e652a96983a5b34040306c74595ebec25
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-06-06 09:49:37 -0400

    added db config

---
+++ Week5/Server/Config/db.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost:27017/media";
+exports.HostName = "localhost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
---
+++ Week5/Server/Config/db.ts
@@ -0,0 +1,3 @@
+export const LocalURI = "mongodb://localhost:27017/media";
+export const HostName = "localhost";
+export const Secret = "someSecret";
