6065acc9de9c5b588bea32547bec133cc037d338
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-06-13 09:15:37 -0400

    added auth stubs

---
+++ Week6/Server/Controllers/auth.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProcessLogoutPage = exports.ProcessRegisterPage = exports.ProcessLoginPage = exports.DisplayRegisterPage = exports.DisplayLoginPage = void 0;
+function DisplayLoginPage(req, res, next) {
+}
+exports.DisplayLoginPage = DisplayLoginPage;
+function DisplayRegisterPage(req, res, next) {
+}
+exports.DisplayRegisterPage = DisplayRegisterPage;
+function ProcessLoginPage(req, res, next) {
+}
+exports.ProcessLoginPage = ProcessLoginPage;
+function ProcessRegisterPage(req, res, next) {
+}
+exports.ProcessRegisterPage = ProcessRegisterPage;
+function ProcessLogoutPage(req, res, next) {
+}
+exports.ProcessLogoutPage = ProcessLogoutPage;
+//# sourceMappingURL=auth.js.map
---
+++ Week6/Server/Controllers/auth.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+
+// need passport functionality
+import passport from 'passport';
+
+// need to include the User model for authentication functions
+import User from '../Models/user';
+
+// Display Functions
+export function DisplayLoginPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    
+}
+
+export function DisplayRegisterPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+   
+}
+
+// Processing Functions
+export function ProcessLoginPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+   
+}
+ 
+export function ProcessRegisterPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    
+}
+
+export function ProcessLogoutPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    
+}
+
+
